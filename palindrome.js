687ef3ecbeec09d0f15f6f86086322f8844c902d
Ankitfozzy <ankitsinghsisodia610@gmail.com>
2022-12-01 16:33:04 +0530

    done

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+let str = "ankit";
+let bag = "";
+
+for(let i=str.length;i>=0;i++)
+{
+  bag+= str[i]; 
+}
+if(bag==str){
+console.log("Yes");
+}
+else{
+console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,10 @@
+let flag = true;
+let N = 13;
+for(let i=0;i<N;i++){
+if(N%i==0){
+console.log("No");
+}
+else{
+console.log("Yes");
+}
+}
