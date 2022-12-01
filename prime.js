7cf2c30118185c0225d35ca08485473f24c5eade
Astha Rajput <ashu00947@gmail.com>
2022-12-01 23:01:43 +0530

    code fix

--- prime.js
+++ prime.js
@@ -1,10 +1,11 @@
-let flag = true;
-let N = 13;
-for(let i=0;i<N;i++){
-if(N%i==0){
-console.log("No");
-}
-else{
-console.log("Yes");
-}
+let N=13
+let count=0
+for(let i=1;i<=N;i++){
+  if(N%i==0){
+    count++;
+  }
+}if(count==2){
+  console.log("yes");
+}else{
+  console.log("No");
 }
